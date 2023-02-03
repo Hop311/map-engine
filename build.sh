c8e284ccd96f18f228da9c871cbc6932d3a0daf3
Hop311 <hop3114@gmail.com>
2023-02-03 21:02:37 +0000

    Improved build script.

--- build.sh
+++ build.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
-set -e
+set +e +x
 
+DIR="build"
+
+RET=0
 echo " === Updating submodules ==="
 git submodule update --init --recursive
+RET=$?
 echo -e " ===========================\n"
-
-echo " === CMake setup ==="
-cmake -S . -B build
-cd build
-echo -e " ===================\n"
-
-echo " === Building ==="
-cmake --build .
-echo -e " ================\n"
-
-echo "Press any key to continue..."
-read -n 1 -s -r
+if [ ${RET} -eq 0 ]; then
+    echo " === CMake setup ==="
+    cmake -S . -B "${DIR}"
+    RET=$?
+    echo -e " ===================\n"
+fi
+if [ ${RET} -eq 0 ]; then
+    echo " === Building ==="
+    cmake --build "${DIR}"
+    RET=$?
+    echo -e " ================\n"
+fi
+if [ ${RET} -eq 0 ]; then echo "*** BUILD SUCCESSFUL ***"
+else echo "*** BUILD FAILED ***"; fi
+read -n 1 -s -r -p $'\nPress any key to continue...\n'
+exit ${RET}
